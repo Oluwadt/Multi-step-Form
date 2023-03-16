b4ae80239c19787087ecaf543fdd693ae7ce36b9
Oluwadt <dtonilede@gmail.com>
2023-03-16 10:28:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,306 @@
+const validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const stepOneForm = document.getElementById('step-one')
+const stepTwo = `<div class="step" id="step-two-div">
+<h1>Select your plan</h1>
+<p class="step-desc">You have the option of monthly or yearly billing</p>
+
+<div class="plan-options">
+  <div class="plan-option" id="arcade-plan">
+    <img src="./assets/images/icon-arcade.svg" class="two-img"><br>
+    <h4>Arcade</h4>
+    <p id="arcade-rate" class="plan-rate">$9/mo</p>
+  </div>
+
+  <div class="plan-option" id="advanced-plan">
+    <img src="./assets/images/icon-advanced.svg" class="two-img"><br>
+    <h4>Advanced</h4>
+    <p id="advanced-rate" class="plan-rate">$12/mo</p>
+  </div>
+
+  <div class="plan-option" id="pro-plan">
+    <img src="./assets/images/icon-pro.svg" class="two-img"><br>
+    <h4>Pro</h4>
+    <p id="pro-rate" class="plan-rate">$15/mo</p>
+  </div>
+</div>
+
+<br>
+
+<div class="select-plan">
+  <p>
+    <p id="monthly">Monthly</p>
+    <label class="switch">
+      <input type="checkbox" id="checkbox">
+      <span class="slider"></span>
+    </label>
+    <p id="yearly">Yearly</p>
+</p>
+</div>
+<p class="back-btn" id="back-one">Go Back</p>
+
+<button id="next-three" class="next-btn">Next Step</button>
+</div>`
+
+const stepOneDiv = document.getElementById('step-one-div')
+const stepTwoDiv = document.getElementById('step-two-div')
+const stepThreeDiv = document.getElementById('step-three-div')
+const stepFourDiv = document.getElementById('step-four-div')
+
+const nextTwo = document.getElementById('next-two')
+const nextThree = document.getElementById('next-three')
+const nextFour = document.getElementById('next-four')
+
+const backOne = document.getElementById('back-one')
+const backTwo = document.getElementById('back-two')
+const backThree = document.getElementById('back-three')
+
+const monthlyText = document.getElementById('monthly')
+const yearlyText = document.getElementById('yearly')
+const switchBtn = document.getElementById('checkbox')
+
+const userName = document.getElementById('name')
+const email = document.getElementById('email')
+const number = document.getElementById('number')
+
+const stepOneButton = document.querySelectorAll('.step-btn')[0]
+const stepTwoButton = document.querySelectorAll('.step-btn')[1]
+const stepThreeButton = document.querySelectorAll('.step-btn')[2]
+const stepFourButton = document.querySelectorAll('.step-btn')[3]
+
+const planOptions = document.querySelectorAll('.plan-option')
+const arcadeBtn = document.getElementById('arcade-plan')
+const advancedBtn = document.getElementById('advanced-plan')
+const proBtn = document.getElementById('pro-plan')
+
+const addOns = document.querySelectorAll('.add-on')
+const storageRate = document.getElementById('storage-rate')
+const onlineRate = document.getElementById('online-rate')
+const customizeRate = document.getElementById('customize-rate')
+
+let selectedPlan = {
+    name: '',
+    rate: ''
+}
+let selectedAddOns = []
+const planName = document.getElementById('plan-name')
+const planRate = document.getElementById('plan-rate')
+const addOnSummary = document.getElementById('add-on-summary')
+
+const changePlan = document.getElementById('change-plan')
+
+let interval = 'Monthly'
+
+stepOneButton.classList.add('current-step-btn')
+monthlyText.style.color = 'hsl(213, 96%, 18%)'
+
+stepOneForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let hasErrors = false
+
+    if (!userName.value) {
+        hasErrors = true
+        const message = `<p class='error'>This field is required</p>`
+        userName.previousElementSibling.outerHTML = message
+        userName.classList.add('error-field')
+    }
+    if (!email.value.match(validRegex)) {
+        hasErrors = true
+        const message = `<p class='error'>This isn't a valid email</p>`
+        email.previousElementSibling.outerHTML = message
+        email.classList.add('error-field')
+    }
+    if (!number.value) {
+        hasErrors = true
+        const message = `<p class='error'>This field is required</p>`
+        number.previousElementSibling.outerHTML = message
+        number.classList.add('error-field')
+    }
+
+    if (!hasErrors) {
+        stepOneDiv.style.display = 'none'
+        stepTwoDiv.style.display = 'block'
+        stepOneButton.classList.remove('current-step-btn')
+        stepTwoButton.classList.add('current-step-btn')
+    }
+})
+
+nextThree.onclick = () => {
+    planOptions.forEach(planOption => {
+        if (planOption.classList.contains('current-plan')) {
+            stepTwoDiv.style.display = 'none'
+            stepThreeDiv.style.display = 'block'
+            stepTwoButton.classList.remove('current-step-btn')
+            stepThreeButton.classList.add('current-step-btn')
+
+            selectedPlan = {
+                name: planOption.children[2].innerHTML,
+                rate: planOption.children[3].innerHTML
+            }
+        } else {
+            null
+        }
+    });
+}
+
+nextFour.onclick = () => {
+    stepThreeDiv.style.display = 'none'
+    stepFourDiv.style.display = 'block'
+    stepThreeButton.classList.remove('current-step-btn')
+    stepFourButton.classList.add('current-step-btn')
+
+    planName.innerText = `${selectedPlan.name}(${interval})`
+    planRate.innerText = selectedPlan.rate
+    
+    addOns.forEach(addOn => {
+        const checkbox = addOn.children[0]
+
+        const name = addOn.children[1].children[0].innerHTML
+        const rate = addOn.children[2].innerHTML
+        const newAddOn = {
+            name,
+            rate
+        }
+        
+        if (checkbox.checked == true) {
+            // console.log(addOn);
+            selectedAddOns.push(newAddOn)
+        }
+    })
+
+    selectedAddOns = selectedAddOns.filter((obj, index, self) =>
+        index === self.findIndex((el) => el.name === obj.name)
+    );
+
+    let tempSelected = ''
+
+    selectedAddOns.forEach(selected => {
+        const addOnDiv = `<div class="selected-add-on">
+        <p class="add-on-name">${selected.name}</p>
+        <p class="summary-add-on-rate">${selected.rate}</p>
+        </div>`
+        tempSelected += addOnDiv
+    });
+    addOnSummary.innerHTML = tempSelected
+}
+
+changePlan.onclick = () => {
+    stepTwoDiv.style.display = 'block'
+    stepFourDiv.style.display = 'none'
+    stepFourButton.classList.remove('current-step-btn')
+    stepTwoButton.classList.add('current-step-btn')
+}
+
+let formElements = document.forms['step-one'].elements
+formElements = Array.from(formElements)
+formElements = formElements.filter(field => field.className === 'input-field')
+
+formElements.forEach(element => {
+    const field = document.getElementById(element.id)
+    field.onchange = () => {
+        field.classList.remove('error-field')
+        if (field.previousElementSibling.className == 'error') {
+            field.previousElementSibling.style.display = 'none'
+        }
+    }
+});
+
+backOne.onclick = () => {
+    stepOneDiv.style.display = 'block'
+    stepTwoDiv.style.display = 'none'
+    stepTwoButton.classList.remove('current-step-btn')
+    stepOneButton.classList.add('current-step-btn')
+}
+
+backTwo.onclick = () => {
+    stepTwoDiv.style.display = 'block'
+    stepThreeDiv.style.display = 'none'
+    stepThreeButton.classList.remove('current-step-btn')
+    stepTwoButton.classList.add('current-step-btn')
+}
+
+backThree.onclick = () => {
+    stepFourDiv.style.display = 'none'
+    stepThreeDiv.style.display = 'block'
+    stepFourButton.classList.remove('current-step-btn')
+    stepThreeButton.classList.add('current-step-btn')
+}
+
+arcadeBtn.onclick = () => {
+    arcadeBtn.classList.add('current-plan')
+    advancedBtn.classList.remove('current-plan')
+    proBtn.classList.remove('current-plan')
+}
+
+advancedBtn.onclick = () => {
+    arcadeBtn.classList.remove('current-plan')
+    advancedBtn.classList.add('current-plan')
+    proBtn.classList.remove('current-plan')
+}
+
+proBtn.onclick = () => {
+    arcadeBtn.classList.remove('current-plan')
+    advancedBtn.classList.remove('current-plan')
+    proBtn.classList.add('current-plan')
+}
+
+switchBtn.onclick = () => {
+    const planRates = document.querySelectorAll('.plan-rate')
+    const arcadeRate = document.getElementById('arcade-rate')
+    const advancedRate = document.getElementById('advanced-rate')
+    const proRate = document.getElementById('pro-rate')
+    if (switchBtn.checked) {
+        monthlyText.style.color = 'hsl(231, 11%, 63%)'
+        yearlyText.style.color = 'hsl(213, 96%, 18%)'
+        arcadeRate.innerText = '$90/yr'
+        advancedRate.innerText = '$120/yr'
+        proRate.innerText = '$150/yr'
+        const twoMonths = `<p class="two-months">2 months free</p>`
+        planRates.forEach(element => {
+            element.parentNode.innerHTML += twoMonths
+        });
+
+        onlineRate.innerText = '+$10/yr'
+        storageRate.innerText = '+$20/yr'
+        customizeRate.innerText = '+$20/yr'
+
+        interval = 'Yearly'
+
+    }
+    else {
+        yearlyText.style.color = 'hsl(231, 11%, 63%)'
+        monthlyText.style.color = 'hsl(213, 96%, 18%)'
+        arcadeRate.innerText = '$9/mo'
+        advancedRate.innerText = '$12/mo'
+        proRate.innerText = '$15/mo'
+        planRates.forEach(element => {
+            if (element.nextElementSibling.className == 'two-months') {
+                element.nextElementSibling.remove()
+            }
+        });
+        arcadeRate.innerText = '$9/mo'
+        advancedRate.innerText = '$12/mo'
+        proRate.innerText = '$15/mo'
+
+        
+        onlineRate.innerText = '+$1/mo'
+        storageRate.innerText = '+$2/mo'
+        customizeRate.innerText = '+$2/mo'
+
+        interval = 'Monthly'
+    }    
+}
+
+addOns.forEach(addOn => {
+    const checkbox = addOn.children[0]
+
+    addOn.onclick = () => {
+        if (checkbox.checked == true) {
+            checkbox.checked = false
+            addOn.classList.remove('current-plan')
+        } else {
+            checkbox.checked = true
+            addOn.classList.add('current-plan')
+        }
+    }
+});
